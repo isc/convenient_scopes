a86c9f996536bdfb2d7f18836886aa19e1b62ad3
Ivan Schneider <contact@ivanschneider.fr>
2011-02-20 23:02:37 +0100

    ignoring .bundle

---
+++ test/test_all_and_any_conditions.rb
@@ -0,0 +1,19 @@
+require 'helper'
+
+class TestAllAndAnyConditions < Test::Unit::TestCase
+  
+  context "convenient_scopes" do
+    
+    setup do
+      @bob = User.create :pseudo => 'Bob Larchi'
+      @slim = User.create :pseudo => 'Slim Babine'
+    end
+    
+    should "handle conditions scopes suffixed by any" do
+      flunk "not implemented yet"
+      assert_equal [@bob, @slim], User.pseudo_like_any('Bob', 'Slim')
+    end
+    
+  end
+  
+end
